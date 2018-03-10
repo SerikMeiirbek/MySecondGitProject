8ea409fe9442b128feca83f2965ed5a813cef42b
sheroziy <sheroziy_uz@yahoo.com>
2018-03-10 14:18:45 -0500

    added statement to Serik

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -4,5 +4,6 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!");
 		System.out.println("My name is Serik");
+		System.out.println("How you doing Serik ? ");// written by Sheroziy
 	}
 }
